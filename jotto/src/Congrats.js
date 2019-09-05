0bf100ddf914acac43763aaead4f8fd73b34ec87
1-8192 <allegranzia@gmail.com>
2019-09-05 11:36:11 -0400

    set up testing suite for congrats component

---
+++ jotto/src/Congrats.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+/**
+ * Functional react component for congratulatory message.
+ * @function 
+ * @returns {JSX.Element} - Rendered component
+ */
+export default () => {
+    return(
+        <div>
+            TEST
+        </div>
+    )
+}
---
+++ jotto/src/Congrats.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+
+import Congrats from './Congrats';
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+const setup = (props) 
+
+test('renders', () => {
+
+});
+
+test('renders no text when `success` props is false', () => {
+
+});
+
+test('renders non-empty congrats message when success prop is true', ()=> {
+
+});
