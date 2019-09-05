cae7ec3d15473eaf15689559da04652dcac91853
1-8192 <allegranzia@gmail.com>
2019-09-05 14:04:07 -0400

    passing test for congrats

--- jotto/src/Congrats.js
+++ jotto/src/Congrats.js
@@ -5,10 +5,18 @@ import React from 'react'
  * @function 
  * @returns {JSX.Element} - Rendered component
  */
-export default () => {
-    return(
-        <div>
-            TEST
-        </div>
-    )
+export default (props) => {
+        if (props.success) {
+            return (
+                <div data-test="component-congrats">
+                <span data-test="congrats-message">
+                    Congratulations! You guessed the word!
+                </span>
+            </div>
+            );
+        } else {
+            return (
+                <div data-test="component-congrats"/>
+            )
+        };
 }
