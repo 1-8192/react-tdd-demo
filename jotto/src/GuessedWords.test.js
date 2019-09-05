670cdfc277cd4e02b76fd7577874407358898df7
1-8192 <allegranzia@gmail.com>
2019-09-05 15:02:04 -0400

    setup GuessedWOrds component and test file

---
+++ jotto/src/GuessedWords.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { findByTestAttr, checkProps} from '../test/testUtils.js'
+
+import GuessedWords from './GuessedWords';
+
+test('renders', () => {
+    
+})
