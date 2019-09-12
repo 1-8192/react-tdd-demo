58952db5a2e2694acb44def77b56ed9df969b156
1-8192 <allegranzia@gmail.com>
2019-09-12 14:47:13 -0400

    Set up guessedWords functions

--- jotto/src/integration.test.js
+++ jotto/src/integration.test.js
@@ -1,6 +1,22 @@
 import { storeFactory } from '../test/testUtils';
 import { guessedWord } from './actions';
+import { tsExternalModuleReference } from '@babel/types';
 
 describe('guessedWord action dispatcher', () => {
+    describe('no guessed words', () => {
+        test('updates state succesfully for unsuccesful guess', () => {
 
+        });
+        test('updates state succesfully for succesful guess', () => {
+
+        });
+    });
+    describe('some guessed words', () => {
+        test('updates state succesfully for unsuccesful guess', () => {
+
+        });
+        test('updates state succesfully for succesful guess', () => {
+
+        });
+    });
 });
