d6cd61f97792d11238305a71bcb0160643e53988
1-8192 <allegranzia@gmail.com>
2019-09-19 15:34:57 -0400

    basic app

--- jotto-hooks/src/App.test.js
+++ jotto-hooks/src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import { findByTestAttr } from '../test/testUtils';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const setup = () => {
+  return shallow(<App />);
+}
+
+test('renders without crashing', () => {
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, 'app-component')
+  expect(appComponent.length).toBe(1);
 });
