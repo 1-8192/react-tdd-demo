2609affe7f55fd6e7167f0250456d0bf6af00e7c
1-8192 <allegranzia@gmail.com>
2019-09-19 15:40:46 -0400

    built input component and test

---
+++ jotto-hooks/src/Input.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { findByTestAttr } from '../test/testUtils';
+
+import Input from './Input';
+
+const setup = () => {
+    return shallow(<Input />);
+};
+
+test('renders component', () => {
+    const wrapper = setup();
+    const inputComponent = findByTestAttr(wrapper, 'input-component');
+    expect(inputComponent.length).toBe(1);
+});
