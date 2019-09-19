4a0568a48c519fbb45b353e0cc1a3c142dd39267
1-8192 <allegranzia@gmail.com>
2019-09-19 15:48:04 -0400

    testing input prop types

--- jotto-hooks/src/Input.test.js
+++ jotto-hooks/src/Input.test.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { findByTestAttr } from '../test/testUtils';
+import { findByTestAttr, checkProps } from '../test/testUtils';
 
 import Input from './Input';
 
-const setup = () => {
-    return shallow(<Input />);
+const setup = (secretWord="party") => {
+    return shallow(<Input secretWord={secretWord}/>);
 };
 
 test('renders component', () => {
     const wrapper = setup();
     const inputComponent = findByTestAttr(wrapper, 'input-component');
     expect(inputComponent.length).toBe(1);
+});
+test('does not throw warning with expected props', () => {
+    checkProps(Input, { secretWord: 'party' } );
 });
