e253b108862ed4c51284665b40a7f848ed543531
1-8192 <allegranzia@gmail.com>
2019-09-20 11:21:31 -0400

    wrote tests for useEffect and coded it in app

--- jotto-hooks/src/App.js
+++ jotto-hooks/src/App.js
@@ -1,7 +1,31 @@
 import React from 'react';
 import './App.css';
+import hookActions from './actions/hookActions';
+
+//reducer to update state needed for hooks
+function reducer(state, action) {
+  switch(action.type) {
+    case "setSecretWord":
+      return { ...state, secretWord: action.payload}
+    default:
+      throw new Error(`invalid action type: ${action.type}`);
+  }
+};
 
 function App() {
+  //hook that uses reducer above
+  const [state, dispatch] = React.useReducer(reducer, { secretWord: "" });
+
+  //function that dispatches in correct format
+  const setSecretWord = (secretWord) => 
+    dispatch({ type: "SetSecretWord", payload: secretWord})
+
+  //empty array makes this run once on mount, basically like ComponentDidMount
+  React.useEffect(
+    () => { hookActions.getSecretWord() },
+    []
+  )
+
   return (
     <div data-test="app-component"/>
   );
