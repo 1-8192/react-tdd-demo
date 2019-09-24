971af73edebd9bf03301541df54bf809fdc54e21
1-8192 <allegranzia@gmail.com>
2019-09-24 10:37:27 -0400

    setup guessedWords context

---
+++ jotto-hooks/src/contexts/guessedWordsContext.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const guessedWordsContext = React.createContext()
+
+export function useGuessedWords() {
+    const context = React.useContext(guessedWordsContext)
+
+    if (!context) {
+        throw new Error('useGuessedWords must be used within provider')
+    }
+
+    return context 
+}
+
+export function guessedWordsProvider(props) {
+    //create state with default empty array
+    const [guessedWords, setGuessedWords] = React.useState([])
+
+    //useMemo stores function return in cache so it only updates when needed
+    const value = React.memo(() => [guessedWords, setGuessedWords], [guessedWords])
+
+    return <guessedWords.Provider value={value} {...props} />
+}
+
+export default { guessedWordsProvider, useGuessedWords }
