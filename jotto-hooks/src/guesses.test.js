ee61f78eef79904ec16e36a6b20a0643d99ec94d
1-8192 <allegranzia@gmail.com>
2019-09-25 10:43:25 -0400

    testing guesses

--- jotto-hooks/src/guesses.test.js
+++ jotto-hooks/src/guesses.test.js
@@ -2,21 +2,32 @@ import React from 'react'
 import { mount } from 'enzyme'
 import { findByTestAttr } from '../test/testUtils'
 
+import guessedWordsContext from './contexts/guessedWordsContext'
 import successContext from './contexts/successContext'
 import Input from './Input'
-import { exportAllDeclaration } from '@babel/types'
+import GuessedWords from './GuessedWords'
 
 //integration tests simulating a word guess
 
-function setup(secretWord = 'party') {
+function setup(guessedWordsStrings=[], secretWord = 'party') {
     const wrapper = mount(
-    <successContext.SuccessProvider>
-        <Input secretWord={secretWord}/>
-    </successContext.SuccessProvider>
+    <guessedWordsContext.GuessedWordsProvider>
+        <successContext.SuccessProvider>
+            <Input secretWord={secretWord}/>
+            <GuessedWords />
+        </successContext.SuccessProvider>
+    </guessedWordsContext.GuessedWordsProvider>
     )
 
     const inputBox = findByTestAttr(wrapper, 'input-box')
     const submitButton = findByTestAttr(wrapper, 'submit-button')
+
+    //prepopulating guessedWords
+    guessedWordsStrings.map(word => {
+        const mockEvent = { target: {value: word} }
+        inputBox.simulate('change', mockEvent)
+        submitButton.simulate('click')
+    })
 
     return [wrapper, inputBox, submitButton]
 }
@@ -25,32 +36,43 @@ describe('word guesses', () => {
     let wrapper
     let inputBox
     let submitButton
-    beforeEach(() => {
-        [wrapper, inputBox, submitButton] = setup("party")
-    })
 
-    describe('correct guess', () => {
+    describe('non empty guessedWords', () => {
         beforeEach(() => {
-            const mockEvent = { target: {value: 'party'} }
-            inputBox.simulate('change', mockEvent)
-            submitButton.simulate('click')
+            [wrapper, inputBox, submitButton] = setup(['agile'], "party")
         })
-        test('Input component contains no children', () => {
-            const inputComponent = findByTestAttr(wrapper, 'input-component')
-
-            expect(inputComponent.children().length).toBe(0)
+    
+        describe('correct guess', () => {
+            beforeEach(() => {
+                const mockEvent = { target: {value: 'party'} }
+                inputBox.simulate('change', mockEvent)
+                submitButton.simulate('click')
+            })
+            test('Input component contains no children', () => {
+                const inputComponent = findByTestAttr(wrapper, 'input-component')
+    
+                expect(inputComponent.children().length).toBe(0)
+            })
+            test('guessedwords table row count reflects update', () => {
+                const guessedWordsTableRows = findByTestAttr(wrapper, 'guessed-word')
+                expect(guessedWordsTableRows.length).toBe(2)
+            })
+        })
+    
+        describe('incorrect guess', () => {
+            beforeEach(() => {
+                const mockEvent = { target: {value: 'train'} }
+                inputBox.simulate('change', mockEvent)
+                submitButton.simulate('click')
+            })
+            test('Input box remains', () => {
+                expect(inputBox.exists()).toBe(true)
+            })
+            test('guessedwords table row count reflects update', () => {
+                const guessedWordsTableRows = findByTestAttr(wrapper, 'guessed-word')
+                expect(guessedWordsTableRows.length).toBe(2)
+            })
         })
     })
-
-    describe('incorrect guess', () => {
-        beforeEach(() => {
-            const mockEvent = { target: {value: 'train'} }
-            inputBox.simulate('change', mockEvent)
-            submitButton.simulate('click')
-        })
-        test('Input box remains', () => {
-            expect(inputBox.exists()).toBe(true)
-        })
-    })
-
+    
 })
